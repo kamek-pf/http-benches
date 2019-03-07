05ded8214a0fd837a9cddaca8895dd9d1268ea31
Bertrand "Kamek" Bousquet <b.kamek@gmail.com>
2019-03-07 18:01:12 -0500

    use standard formatters everywhere

--- warp_hello/src/main.rs
+++ warp_hello/src/main.rs
@@ -2,9 +2,7 @@ use warp::{self, path, Filter};
 
 fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = path!("hello" / String)
-        .map(|name| format!("Hello {}!", name));
+    let hello = path!("hello" / String).map(|name| format!("Hello {}!", name));
 
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 8080));
+    warp::serve(hello).run(([127, 0, 0, 1], 8080));
 }
